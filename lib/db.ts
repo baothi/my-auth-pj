7a0879ee6ad36e2cfe7180af14b5b2cb5af8b005
baothi <baothi246@gmail.com>
2024-07-23 20:56:52 +0700

    commit auth

--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,12 @@
-import mongoose from 
+import mongoose from 'mongoose';
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI!);
+        console.log('Succeeded to connected to MongooDB');
+    } catch (error: any) {
+        console.error(`Error: ${error.message}`);
+    }
+}
+
+export default connectDB;
